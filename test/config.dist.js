a7157f768010393cb38d213176ccd99aab82c543
Espen Hovlandsdal <rexxars@gmail.com>
2012-10-12 16:19:35 +0200

    Removed unused config-file.

--- test/config.dist.js
+++
@@ -1,6 +0,0 @@
-exports.imgPath  = __dirname + '/cat.jpg';
-exports.imbo = {
-    'hosts'     : ['http://imbo.somehost.com'],
-    'privateKey': 'abcdefghijklmnopqrstuvwxyz123456',
-    'publicKey' : '654321abcdefghijklmnopqrstuvwxyz'
-};
