b6f4d722dff4d223320b3c2cbd1d7dec21d410db
Espen Hovlandsdal <espen@hovlandsdal.com>
2013-10-20 17:26:21 -0700

    Added conditional outputting of httpd log

--- test/travis.sh
+++ test/travis.sh
@@ -3,6 +3,14 @@ php composer.phar create-project -n imbo/imbolauncher imbolauncher dev-develop
 ./imbolauncher/imbolauncher start-servers --config=$TRAVIS_BUILD_DIR/test/imbolauncher/config.json --install-path=/tmp/imbo-servers --no-interaction -vvv ; cat /tmp/imbo-servers/0.3.3/httpd.log
 make test
 OUT=$?
+
+if [ $OUT -eq 0 ];then
+    echo "Tests successful!"
+else
+    echo "Tests failed, httpd log: "
+    cat /tmp/imbo-servers/0.3.3/httpd.log
+fi
+
 ./imbolauncher/imbolauncher kill-servers --no-interaction -vvv
 
 exit $OUT
