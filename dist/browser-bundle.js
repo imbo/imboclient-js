578e54993277b50ed8ec649f963f91bfea575bdf
Espen Hovlandsdal <espen@hovlandsdal.com>
2013-11-06 10:15:32 +0100

    Added tests for feature support.

--- dist/browser-bundle.js
+++ dist/browser-bundle.js
@@ -107,23 +107,40 @@ module.exports = {
     }
 };
 },{"./md5.min":4,"./readers":5,"./sha":7,"__browserify_process":11}],3:[function(require,module,exports){
+/**
+ * This file is part of the imboclient-js package
+ *
+ * (c) Espen Hovlandsdal <espen@hovlandsdal.com>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
 'use strict';
 
-exports.getUnsupported = function() {
-    var unsupported = [];
-    if (!window.FileReader) {
+exports.checkFeatures = function(context) {
+    if (typeof window !== 'undefined' || context) {
+        var unsupported = exports.getUnsupported(context);
+        if (unsupported.length) {
+            throw new Error('Browser does not support ' + unsupported.join(', '));
+        }
+    }
+};
+
+exports.getUnsupported = function(context) {
+    var global = context || window
+      , unsupported = [];
+
+    if (!global.FileReader) {
         unsupported.push('FileReader');
     }
 
-    if (!window.ArrayBuffer) {
+    if (!global.ArrayBuffer) {
         unsupported.push('ArrayBuffer');
     }
 
-    if (!window.XMLHttpRequest) {
+    if (!global.XMLHttpRequest) {
         unsupported.push('XMLHttpRequest');
-    }
-
-    if (!('upload' in new XMLHttpRequest())) {
+    } else if (!('upload' in new global.XMLHttpRequest())) {
         unsupported.push('XMLHttpRequest2');
     }
 
@@ -422,12 +439,7 @@ var ImboClient = function(serverUrls, publicKey, privateKey) {
         privateKey: privateKey
     };
 
-    if (typeof window !== 'undefined') {
-        var unsupported = features.getUnsupported();
-        if (unsupported.length) {
-            throw new Error('Browser does not support ' + unsupported.join(', '));
-        }
-    }
+    features.checkFeatures();
 };
 
 /**
---
+++ test/unit/feature-support.js
@@ -0,0 +1,59 @@
+var features = require('../../lib/browser/feature-support')
+  , assert   = require('assert');
+
+describe('feature-support', function() {
+
+    var unsupported;
+
+    describe('#getUnsupported', function() {
+
+        it('should check for FileReader', function() {
+            unsupported = features.getUnsupported({});
+            assert.notEqual(-1, unsupported.indexOf('FileReader'));
+
+            unsupported = features.getUnsupported({ FileReader: true });
+            assert.equal(-1, unsupported.indexOf('FileReader'));
+        });
+
+        it('should check for ArrayBuffer', function() {
+            unsupported = features.getUnsupported({});
+            assert.notEqual(-1, unsupported.indexOf('ArrayBuffer'));
+
+            unsupported = features.getUnsupported({ ArrayBuffer: true });
+            assert.equal(-1, unsupported.indexOf('ArrayBuffer'));
+        });
+
+        it('should check for XMLHttpRequest', function() {
+            unsupported = features.getUnsupported({});
+            assert.notEqual(-1, unsupported.indexOf('XMLHttpRequest'));
+        });
+
+        it('should check for XMLHttpRequest2', function() {
+            unsupported = features.getUnsupported({ 'XMLHttpRequest': function() {} });
+            assert.notEqual(-1, unsupported.indexOf('XMLHttpRequest2'));
+
+            var XMLHttpRequest = function() {};
+            XMLHttpRequest.prototype.upload = {};
+
+            unsupported = features.getUnsupported({ 'XMLHttpRequest': XMLHttpRequest });
+            assert.equal(-1, unsupported.indexOf('XMLHttpRequest2'));
+        });
+
+    });
+
+    describe('#checkFeatures', function() {
+
+        it('should throw Error on unsupported features', function() {
+            var shouldThrow = function() {
+                features.checkFeatures({
+                    FileReader: true,
+                    ArrayBuffer: true
+                });
+            };
+
+            assert.throws(shouldThrow, Error, 'Error thrown');
+        });
+
+    });
+
+});
