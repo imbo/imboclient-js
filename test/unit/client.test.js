a8cd350288105ca22773c2734a0939974f2caff3
Espen Hovlandsdal <rexxars@gmail.com>
2015-09-29 16:04:41 +0200

    Accept user in client constructor options

--- test/unit/client.test.js
+++ test/unit/client.test.js
@@ -41,6 +41,44 @@ describe('ImboClient', function() {
         mockImgUrl.done();
     });
 
+    describe('#constructor()', function() {
+        it('should throw on invalid hosts', function() {
+            assert.throws(function() {
+                client = new Imbo.Client({});
+            }, /hosts/);
+
+            assert.throws(function() {
+                client = new Imbo.Client({ hosts: {} });
+            }, /hosts/);
+        });
+
+        it('should throw on invalid publicKey', function() {
+            assert.throws(function() {
+                client = new Imbo.Client({ hosts: ['foo'], publicKey: '' });
+            }, /publicKey/);
+
+            assert.throws(function() {
+                client = new Imbo.Client({ hosts: ['foo'], publicKey: [] });
+            }, /publicKey/);
+        });
+
+        it('should throw on invalid privateKey', function() {
+            assert.throws(function() {
+                client = new Imbo.Client({ hosts: ['foo'], publicKey: 'foo', privateKey: '' });
+            }, /privateKey/);
+
+            assert.throws(function() {
+                client = new Imbo.Client({ hosts: ['foo'], publicKey: 'foo', privateKey: [] });
+            }, /privateKey/);
+        });
+
+        it('should throw on invalid user', function() {
+            assert.throws(function() {
+                client = new Imbo.Client({ hosts: ['foo'], publicKey: 'foo', privateKey: 'bar', user: [] });
+            }, /user/);
+        });
+    });
+
     describe('#getServerStatus()', function() {
         it('should return error on a 503-response', function(done) {
             mock.filteringPath(signatureCleaner)
@@ -403,34 +441,6 @@ describe('ImboClient', function() {
                 assert.equal('http://imbo1', client.getHostForImageIdentifier('61ca9892205a0d5077a353eb3487e8c8'));
                 assert.equal('http://imbo', client.getHostForImageIdentifier('3faab4bb128b56bd7d7e977164b3cc7f'));
             }
-        });
-    });
-
-    describe('#parseUrls()', function() {
-        it('should handle being passed a server-string', function() {
-            var urls = client.parseUrls('http://imbo');
-            assert.equal(1, urls.length);
-            assert.equal('http://imbo', urls[0]);
-        });
-
-        it('should handle being passed an array', function() {
-            var hosts = ['http://imbo01', 'http://imbo02', 'http://imbo03'];
-            var urls = client.parseUrls(hosts), host = hosts.length;
-            assert.equal(3, urls.length);
-
-            while (host--) {
-                assert.equal(hosts[host], urls[host]);
-            }
-        });
-
-        it('should strip trailing slashes', function() {
-            assert.equal('http://imbo', client.parseUrls('http://imbo/')[0]);
-            assert.equal('http://imbo/some/path', client.parseUrls('http://imbo/some/path/')[0]);
-        });
-
-        it('should strip port 80', function() {
-            assert.equal('http://imbo', client.parseUrls('http://imbo/:80')[0]);
-            assert.equal('http://imbo/some/path', client.parseUrls('http://imbo:80/some/path/')[0]);
         });
     });
 
